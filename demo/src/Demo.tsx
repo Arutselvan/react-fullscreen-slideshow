40bda97d4ddb7a77f747d583df2173e03907bac6
Arutselvan <arutselvan710@gmail.com>
2020-05-17 04:10:54 +0530

    refactor(demo): Stylize demo page

--- demo/src/Demo.tsx
+++ demo/src/Demo.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
 import ReactFullscreenSlideshow from '../../src/index';
 import {GalleryImages} from './GalleryImages';
+import './css/demo.css';
 
 export default class Demo extends React.Component{
 
     
     render() {
         return(
-            <div>
-				<ReactFullscreenSlideshow images={GalleryImages} title={'Sports Gallery'} />
+            <div className="container">
+                <div className="title">
+                    <h1>react-fullscreen-slideshow</h1>
+                </div>
+                <div className="slideshow">
+                    <ReactFullscreenSlideshow images={GalleryImages} currentSlideIndex={0} title={'Sports Gallery'}/>
+                </div>
             </div>
         );
     }
