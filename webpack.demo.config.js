2eaa925fc1412c52e939801daa40ad52f278405c
Arutselvan <arutselvan710@gmail.com>
2020-05-16 19:46:07 +0530

    chore: Add webpack for demo

---
+++ webpack.demo.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+	entry: "./demo/src/index",
+	output: {
+		path: path.join(__dirname, "./demo/js"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".jsx"],
+		modules: ["node_modules"],
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ["file-loader?name=../assets/images/[name].[ext]"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./demo/index.html",
+		}),
+	],
+};
